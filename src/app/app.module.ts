1b0dd79f7d028de22ed36424011134c704e09887
Cyril-Pulsar <cyril.verdad@pulsar-sd.com>
2023-10-23 12:25:31 +0800

    added housing location list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,11 @@ import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
