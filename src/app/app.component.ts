2c981f46543860b4ac52ecd4eead9d9dbc2b89b1
Cyril-Pulsar <cyril.verdad@pulsar-sd.com>
2023-10-23 13:39:12 +0800

    Revert "added routing and details route"
    This reverts commit 55ff6af6805f7e7cb26dd615dbbc8f41a0d0348f.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.scss'],
-  standalone: true,
   template: `
     <main>
-      <a [routerLink]="['/']">
-        <header class="brand-name">
-          <img
-            class="brand-logo"
-            src="/assets/logo.svg"
-            alt="logo"
-            aria-hidden="true"
-          />
-        </header>
-      </a>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        />
+      </header>
       <section class="content">
-        <router-outlet></router-outlet>
+        <app-home></app-home>
       </section>
     </main>
   `,
-  imports: [HomeComponent, RouterModule],
 })
 export class AppComponent {
   title = 'First-Angular-Project';
